470fe91540231d5f374d8d1e79cd7b9d5d60a034
NekokeCore <core2002@163.com>
2022-01-18 21:40:41 +0800

    添加项目文件。

---
+++ FiFu书编辑器/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace FiFu书编辑器
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.MaxLength = 256;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(481, 473);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "上一页";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(394, 491);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "下一页";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(499, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "打开文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 496);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "页码";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(499, 52);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 39);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "保存";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(499, 97);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 33);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "另存为";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(499, 375);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 35);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "添加页";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(499, 419);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 31);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "删除页";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 535);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "FiFu 书编辑器 By.钟小白Core";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ FiFu书编辑器/Form1.cs
@@ -0,0 +1,197 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FiFu书编辑器
+{
+    public partial class Form1 : Form
+    {
+        static FiFuBook 正在浏览的书;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            正在浏览的书.change(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择书文件";
+            dialog.Filter = "书文件(*.txt)|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = dialog.FileName;
+                正在浏览的书 = new FiFuBook(file);
+                textBox1.Text = 正在浏览的书.getPage(0);
+                label1.Text = 正在浏览的书.getPageNumText();
+
+                this.Text = "FiFu 书编辑器 - " + 正在浏览的书.path;
+
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                textBox1.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text=正在浏览的书.previousPage();
+            label1.Text = 正在浏览的书.getPageNumText();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = 正在浏览的书.nextPage();
+            label1.Text = 正在浏览的书.getPageNumText();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();  //实例化保存文件对话框对象
+            saveFile.Title = "请选择保存文件路径";
+            saveFile.Filter = "书文件(*.txt)|*.txt";
+            saveFile.OverwritePrompt = true;  //是否覆盖当前文件
+            saveFile.RestoreDirectory = true;  //还原目录
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                // MessageBox.Show(saveFile.FileName);
+                File.WriteAllText(saveFile.FileName, 正在浏览的书.toJsonText());
+                // MessageBox.Show("另存文档成功");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(正在浏览的书.path, 正在浏览的书.toJsonText());
+            // MessageBox.Show("另存文档成功");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            正在浏览的书.addPage();
+            label1.Text = 正在浏览的书.getPageNumText();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            正在浏览的书.delPage();
+            label1.Text = 正在浏览的书.getPageNumText();
+            textBox1.Text = 正在浏览的书.getPage();
+        }
+    }
+
+
+    public class FiFuBook
+    {
+        List<string> book;
+        int[] p = { 0,0};
+        public string path;
+        public FiFuBook(string bookFile)
+        {
+            var f = File.OpenText(bookFile);
+            String bookJson = f.ReadToEnd();
+            book = JsonConvert.DeserializeObject<List<string>>(bookJson);
+            path = bookFile;
+            p[0] = 0;
+            p[1] = getMaxPageNum();
+            f.Close();
+        }
+
+        public string toJsonText()
+        {
+            return JsonConvert.SerializeObject(book);
+        }
+
+        public string getPage(int page)
+        {
+            return book[page];
+        }
+
+        public string getPage()
+        {
+            return book[p[0]];
+        }
+
+        public int getMaxPageNum()
+        {
+            return book.Count();
+        }
+
+        public string nextPage()
+        {
+            ++p[0];
+            if (p[0] < p[1] && p[0] >= 0)
+            {
+                return getPage(p[0]);
+            }
+            else
+            {
+                p[0]=0;
+                return getPage(p[0]);
+            }
+        }
+
+        public string previousPage()
+        {
+            --p[0];
+            if (p[0] < p[1] && p[0] >= 0)
+            {
+                return getPage(p[0]);
+            }
+            else
+            {
+                p[0] = p[1]-1;
+                return getPage(p[0]);
+            }
+        }
+
+        public void change(String text) {
+            book[p[0]] = text;       
+        }
+
+        public void addPage()
+        {
+            book.Add("");
+            p[1] = getMaxPageNum();
+        }
+
+        public void delPage()
+        {
+            if (p[0] == 0)
+            {
+                book[0] = "";
+                return;
+            }
+            book.Remove(getPage(p[0]));
+            p[1] = getMaxPageNum();
+            previousPage();
+        }
+
+        public string getPageNumText()
+        {
+            return "第 " + (p[0]+1) + " 页/共 " + p[1] + " 页";
+        }
+    }
+}
